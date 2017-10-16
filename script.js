18214bab0527e1eb3da500de9911e7c0d0f836c2
Mariana Beilune Abad <maryh.abad@gmail.com>
2017-10-16 17:19:55 -0200

    novo

---
+++ script.js
@@ -0,0 +1,21 @@
+function iconeManager1() {/*HOME*/
+    document.getElementById("titulo").innerHTML = "Sobre o Resumão";
+    document.getElementById("faixa").src = "img/resumao.png";
+    document.getElementById("texto").innerHTML = "O {re}sumão foi montado com o propósito de ajudar todas as meninas que participaram da turma 4 do Bootcamp de FrontEnd do <strong>{reprograma}</strong>. Facilitando o acesso à informação de forma clara e objetiva.";
+}
+
+function iconeManager2() {
+    document.getElementById("titulo").innerHTML = "Sobre FrontEnd";
+    document.getElementById("faixa").src = "img/FrontEnd.png";
+    document.getElementById("texto").innerHTML = "O desenvolvedor FrontEnd é responsável por 'dar vida' à interface. Trabalha com a parte da aplicação que interage diretamente com o usuário. Por isso, é importante que esse desenvolvedor também se preocupe com a experiência do usuário. Na parte de estudo, este profissional foca em HTML (linguagem de marcação), CSS (linguagem de estilo) e JavaScript (linguagem de script/programação)..";
+}
+function iconeManager3() {
+    document.getElementById("titulo").innerHTML = "Sobre HTML/CSS";
+    document.getElementById("faixa").src = "img/codigo.png"
+    document.getElementById("texto").innerHTML = "HTML (abreviação para a expressão inglesa HyperText Markup Language, que significa Linguagem de Marcação de Hipertexto) é uma linguagem de marcação utilizada na construção de páginas na Web. Documentos HTML podem ser interpretados por navegadores. A tecnologia é fruto da junção entre os padrões HyTime e SGML.<br><br>Cascading Style Sheets (CSS) é um simples mecanismo para adicionar estilo (cores, fontes, espaçamento, etc.) a um documento web. Em vez de colocar a formatação dentro do documento, o CSS cria um link para uma página que contém os estilos. Quando quiser alterar a aparência do portal, basta modificar apenas um arquivo. Com a variação de atualizações dos navegadores, o suporte ao CSS pode variar. O Internet Explorer 6, p. ex., tem suporte total a CSS1, e praticamente nulo a CSS2. Navegadores mais modernos, como Google Chrome e Mozilla Firefox, tem suporte maior, inclusive até a CSS3, ainda em desenvolvimento.";
+}
+
+function iconeManager4() { /*TAG*/
+    document.getElementById("texto").innerHTML = "<iframe src='https://pastebin.com/embed_iframe/2VwyTLHZ' style='border:none;width:100%;height:300px;margin-top:10%'></iframe>";
+}
+
