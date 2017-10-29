37458c9f4dc89f11d448f0219c0239cb7813f670
Mariana Beilune Abad <maryh.abad@gmail.com>
2017-10-29 00:23:51 -0200

    Login

--- js/cadastro.js
+++ js/cadastro.js
@@ -4,6 +4,8 @@ var botaoLogin = document.getElementById("#login");
 
 var fundoCadastro = document.getElementById("fundoCadastro");
 var fecharCadastro = document.getElementById("fecharCadastro");
+
+var boas = document.querySelector(".boas");
 // var usuario;
 // var email = document.getElementById('email');
 // var senha = document.querySelector('#senha');
@@ -12,7 +14,13 @@ var fecharCadastro = document.getElementById("fecharCadastro");
 var formulario = document.querySelector('form');
 
 login.onclick = function() {
-    fundoLogin.classList.add("visivel");
+    if (botaoLogin.value == "Deslogar") {
+        localStorage.clear();
+        boas.innerHTML = "Seja bem vinda, faça <strong>login</strong> para começar";
+    
+    } else {
+        fundoLogin.classList.add("visivel");
+    }
 }
 
 fundoLogin.onclick = function(e) {
@@ -25,11 +33,6 @@ fundoLogin.onclick = function(e) {
 fecharLogin.onclick = function() {
     fundoLogin.classList.remove("visivel");  
 }
-
-
-
-
-
 
 cadastro.onclick = function() {
     fundoCadastro.classList.add("visivel");
@@ -53,32 +56,25 @@ formCadastro.onsubmit = function (e) {
     var nome = document.querySelector("#nomeCadastro");
     var emailCadastro = document.querySelector("#emailCadastro");
     var senhaCadastro = document.querySelector("#senhaCadastro");
-    var confirmacao = document.querySelector("#confirmacaoCadastro");
-    var boas = document.querySelector(".boas");
-
+    var confirmacao = document.querySelector("#confirmacaoCadastro"); 
     var modalCadastro = document.querySelector(".blocoCadastro");
+    var botaoCadastro = document.querySelector("#cadastro");
 
-    var usuarios = JSON.parse(localStorage.usuarios || "[]");
 
     localStorage.nome = nome.value;
     localStorage.email = emailCadastro.value;
     localStorage.senha = senhaCadastro.value;
     localStorage.confirmacao = confirmacao.value;
 
-    usuarios.push({
-        email: emailCadastro.value,
-        senha: senhaCadastro.value,
-        nome: nome.value,
-    });
 
     if (senhaCadastro.value !== confirmacao.value) {
         alert("As duas senhas não correspondem, tente novamente");
     } else {
         fundoCadastro.classList.remove("visivel");
-        boas.innerHTML = "Seja bem vinda, " +nome.value;
+        boas.innerHTML = "Seja bem vinda, <strong>" +nome.value+ "</strong>";
+        botaoCadastro.classList.add("invisivel");
         e.preventDefault();
     }
-    localStorage.usuarios = JSON.stringify(usuarios);
 }
 
 var formLogin = fundoLogin.querySelector('form');
@@ -87,20 +83,16 @@ formLogin.onsubmit = function(e) {
     var senhaLogin = document.querySelector("#senhaLogin");
 
     var modalLogin = document.querySelector(".blocoLogin");
-    var boas = document.querySelector(".boas");
-    var usuarios = JSON.parse(localStorage.usuarios || "[]");
     var correto;
-    for (var i = 0; i < usuarios.length; i++) {
-        if (usuarios[i].email == emailLogin.value && usuarios[i].senha == senhaLogin.value) {
+        if (localStorage.email == emailLogin.value && localStorage.senha == senhaLogin.value) {
     //troca de página
             fundoLogin.classList.remove("visivel");
-            boas.innerHTML = "Seja bem vinda, " +usuarios[i].nome;
+            boas.innerHTML = "Seja bem vinda, <strong>" +localStorage.nome+ "</strong>";
             e.preventDefault();
         } else {
             alert("Seus dados estão incorretos, tente novamente");
             return false;
         }
-    }
     
     
 }
--- js/novapg.js
+++ js/novapg.js
@@ -1,4 +1,13 @@
 var nome = localStorage.nome;
-var boas = document.querySelector(".boas");
+var botaoLogin = document.querySelector("#login");
+var botaoCadastro = document.querySelector("#cadastro");
 
-boas.innerHTML = "Seja bem vinda, <strong>" +nome+ "</strong>";
+if (nome == null) {
+    boas.innerHTML = "Seja bem vinda, faça <strong>login</strong> para começar";
+    botaoCadastro.classList.remove("invisivel");
+} else {
+    boas.innerHTML = "Seja bem vinda, <strong>" +nome+ "<strong>";
+    botaoLogin.value = "Deslogar";
+}
+
+
